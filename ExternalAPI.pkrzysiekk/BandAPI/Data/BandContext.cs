d8e80487f7fc1748754e6686ff00aab1bbcdeddd
pkrzysiek <pkrzysiek24@gmail.com>
2025-06-24 18:11:36 +0200

    Add working URLs

--- ExternalAPI.pkrzysiekk/BandAPI/Data/BandContext.cs
+++ ExternalAPI.pkrzysiekk/BandAPI/Data/BandContext.cs
@@ -19,49 +19,49 @@ public class BandContext : DbContext
    {
     Name = "Linkin Park",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/commons/3/30/Linkin_Park_logo.svg"
+    imageURL = "https://w0.peakpx.com/wallpaper/215/379/HD-wallpaper-linkin-park-linkinparklogo.jpg"
    },
    new Band
    {
     Name = "Korn",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/commons/5/56/Korn_Logo.svg"
+    imageURL = "https://1000marcas.net/wp-content/uploads/2020/02/Logo-Korn-768x480.png"
    },
    new Band
    {
     Name = "Limp Bizkit",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/en/f/fc/Limpbizkit.png"
+    imageURL = "https://1000logos.net/wp-content/uploads/2020/04/Limp-Bizkit-Logo.png"
    },
    new Band
    {
     Name = "Slipknot",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Slipknot_-_Logo.svg/1280px-Slipknot_-_Logo.svg.png"
+    imageURL ="https://wallpaperaccess.com/full/1617294.jpg"
    },
    new Band
    {
     Name = "P.O.D.",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/en/7/78/P.O.D._logo.svg"
+    imageURL = "https://logodix.com/logo/2086957.png"
    },
    new Band
    {
     Name = "Papa Roach",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Papa_Roach_logo.png"
+    imageURL = "https://logodix.com/logo/748862.jpg"
    },
    new Band
    {
     Name = "Deftones",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/en/7/7a/Deftones_-_Logo.svg"
+    imageURL = "https://logodix.com/logo/2094590.jpg"
    },
    new Band
    {
     Name = "Disturbed",
     Genre = "Nu Metal",
-    imageURL = "https://upload.wikimedia.org/wikipedia/en/6/6b/Disturbed-logo.svg"
+    imageURL = "https://1000logos.net/wp-content/uploads/2017/09/Emblem-Disturbed-768x644.jpg"
    }
   };
   var items=this.Bands.ToList();
